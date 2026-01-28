a2bee8e680f982d755bdb3446e451142a5f09287
Venkata Mani Siddhu Padala <siddhupadala163@gmail.com>
2026-01-28 22:57:52 +0530

    Navbar & Hero

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+if (menuToggle) {
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+}
